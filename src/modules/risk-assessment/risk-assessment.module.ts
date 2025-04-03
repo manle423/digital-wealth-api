1d21d34dbf7c24eca78804a0c2f9d27914f2136e
manle423 <trungmanle.vm@gmail.com>
2025-04-04 02:18:25 +0700

    fix lack of connection name

--- src/modules/risk-assessment/risk-assessment.module.ts
+++ src/modules/risk-assessment/risk-assessment.module.ts
@@ -5,19 +5,24 @@ import { AssessmentResult } from './entities/assessment-result.entity';
 import { RiskAssessmentController } from './risk-assessment.controller';
 import { RiskAssessmentService } from './risk-assessment.service';
 import { UserModule } from '../user/user.module';
+import { LoggerModule } from '@/shared/logger/logger.module';
 import { QuestionRepository } from './repositories/question.repository';
 import { AssessmentResultRepository } from './repositories/assessment-result.repository';
+import { JwtService } from '@nestjs/jwt';
+import { MysqldbConnection } from '@/shared/mysqldb/connections/db.connection';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Question, AssessmentResult]),
-    UserModule
+    TypeOrmModule.forFeature([Question, AssessmentResult], MysqldbConnection.name),
+    UserModule,
+    LoggerModule
   ],
   controllers: [RiskAssessmentController],
   providers: [
     RiskAssessmentService,
     QuestionRepository,
-    AssessmentResultRepository
+    AssessmentResultRepository,
+    JwtService
   ],
   exports: [RiskAssessmentService],
 })
